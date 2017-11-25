0aff9765ce29a7dde43484061cda565178b3ec00
lcdtyph <lcdtyph@gmail.com>
2017-11-25 18:53:29 +0800

    check privilege when startup

--- MyIpsec/main.cpp
+++ MyIpsec/main.cpp
@@ -1,11 +1,22 @@
 #include "myipsec.h"
 #include <QApplication>
+#include <QString>
+#include <QMessageBox>
+#include <memory>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MyIpsec w;
-    w.show();
+    std::unique_ptr<MyIpsec> w;
+    if (getuid() != 0) {
+        QMessageBox::warning(nullptr, "Error!",
+                             "MyIpsec needs root permission",
+                             QMessageBox::Ok);
+        return 0;
+    }
+    w.reset(new MyIpsec);
+    w->show();
 
     return a.exec();
 }
